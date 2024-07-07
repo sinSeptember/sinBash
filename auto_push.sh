4b08b3d25757389548ba44076e0937158f8ec207
September <salbibar@gmail.com>
2024-07-07 11:20:12 +0300

    Update auto_push.sh

--- auto_push.sh
+++ auto_push.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Define variables
 local_repo_path="Path/to/directory"  
 commit_message="Update from script"         
 remote_name="origin"                        
 branch_name="master"                          
-
-# Change directory to the local repository
 cd "$local_repo_path" || exit
 
-# Add all changes and commit
 git add .
 git commit -m "$commit_message"
 
-# Push changes to remote repository
 git push "$remote_name" "$branch_name"
 
-# Optionally, you can add additional logic here, such as error handling or notifications
+#To-Do add additional logic here, such as error handling or notifications
 
